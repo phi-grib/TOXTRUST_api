bc74120b1422f3f9596f685f5194362d2c699ee3
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-05-23 16:21:25 +0200

    create,delete endpoint added

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from settings import *
 from toxtrust import manage
+import json
 
 # GET LIST OF ENDPOINTS
 @app.route(f'{url_base}{version}list',methods=['GET'])
@@ -8,3 +9,18 @@ def getListEndpoints():
     data = manage.listEndpoints()
     print(data)
     return data
+
+#CREATE ENDPOINT
+@app.route(f'{url_base}{version}new/<string:endpoint_name>',methods=['PUT'])
+@cross_origin()
+def createEndpoint(endpoint_name):
+    manage.createEndpoint(endpoint_name)
+    return json.dumps({'success':True}),200,{'ContentType':'application/json'}
+
+#DELETE ENDPOINT
+@app.route(f'{url_base}{version}delete/<string:endpoint_name>',methods=['PUT'])
+@cross_origin()
+def deleteEndpoint(endpoint_name):
+    manage.removeEndpoint(endpoint_name)
+    return json.dumps({'success':True}),200,{'ContentType':'application/json'}
+
