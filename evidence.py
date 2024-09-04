ef7d8de7995747f5f2eb3e8f7c44162361c4391f
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-09-04 17:16:19 +0200

    new endpoint image path

--- evidence.py
+++ evidence.py
@@ -1,4 +1,4 @@
-from flask import request,json
+from flask import request,json,send_file
 from settings import *
 from toxtrust import flask
 import json
@@ -16,3 +16,15 @@ def getEvidenceResult(endpoint_name,evidence_name):
 def deleteEvidence(endpoint_name,evidence_name):
     success,data = flask.removeEvidence(endpoint_name,evidence_name)
     return json.dumps({'success':success,'data':data}),200,{'ContentType':'application/json'}
+
+#GET IMAGE PATH
+@app.route(f'{url_base}{version}evidence_image_path/<string:endpoint_name>/<string:evidence_name>',methods=['GET'])
+@cross_origin()
+def getEvidenceImagePath(endpoint_name,evidence_name):
+    success,data = flask.pathEvidencePlot(endpoint_name,evidence_name)
+    if success:
+        print("aqui esta")
+        print(data)
+        return send_file(data,as_attachment=True)
+    else:
+        return json.dumps(f'Failed to get link {link_name}, with error {repo_path}'), 500, {'ContentType':'application/json'} 
