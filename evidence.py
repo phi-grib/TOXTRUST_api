3e281b3633143b9513cdbdefec121d3c5733ca8d
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-09-28 17:10:34 +0200

    bug fixed

--- evidence.py
+++ evidence.py
@@ -7,7 +7,7 @@ import json
 @app.route(f'{url_base}{version}evidence_result/<string:endpoint_name>/<string:evidence_name>',methods=['GET'])
 @cross_origin()
 def getEvidenceResult(endpoint_name,evidence_name):
-    success,data = flask.returnComputedResult(endpoint_name,evidence_name,selection)
+    success,data = flask.returnComputedResult(endpoint_name,evidence_name)
     return json.dumps({'success':success,'data':data}),200,{'ContentType':'application/json'}
 
 #DELETE EVIDENCE
