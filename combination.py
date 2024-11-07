23594aa80c0aedab1e4738f9320eadee9be6c564
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-11-07 12:49:53 +0100

    new endpoint combination graph added

--- combination.py
+++ combination.py
@@ -2,8 +2,7 @@ from flask import request,json,send_file
 from settings import *
 from toxtrust import flask
 import json
-
-
+from toxtrust import flask
 # SELECT RULE
 @app.route(f'{url_base}{version}select_rule/<string:endpoint_name>',methods=['POST'])
 @cross_origin()
@@ -31,17 +30,17 @@ def runCombine(endpoint_name):
     return json.dumps({'success':success,'message':message}),200,{'ContentType':'application/json'}
 
 
-#PATH COMBINATION IMAGE
-@app.route(f'{url_base}{version}combination_image_path/<string:endpoint_name>',methods=['GET'])
-@cross_origin()
-def getCombinationImagePath(endpoint_name):
-    success,data = flask.pathCombinationPlot(endpoint_name)
-    print("combination path")
-    print(success)
-    if success:
-        return send_file(data,as_attachment=True)
-    else:
-        return json.dumps(f'Failed to get link'), 500, {'ContentType':'application/json'} 
+#PATH COMBINATION IsMAGE
+# @app.route(f'{url_base}{version}combination_image_path/<string:endpoint_name>',methods=['GET'])
+# @cross_origin()
+# def getCombinationImagePath(endpoint_name):
+    # success,data = flask.pathCombinationPlot(endpoint_name)
+    # print("combination path")
+    # print(success)
+    # if success:
+        # return send_file(data,as_attachment=True)
+    # else:
+        # return json.dumps(f'Failed to get link'), 500, {'ContentType':'application/json'} 
 
 #Weight of evidence
 @app.route(f'{url_base}{version}WoEinput/<string:endpoint_name>/<string:WoE>',methods=['GET'])
@@ -53,4 +52,14 @@ def shouldWoeInput(endpoint_name,WoE):
         WoE = False
     success,message = flask.shouldWoeInput(endpoint_name,WoE)
     return json.dumps({'success':success,'message':message}),200,{'ContentType':'application/json'}
+
+# combination plot
+@app.route(f'{url_base}{version}combination_data/<string:endpoint_name>',methods=['GET'])
+@cross_origin()
+def get_combination_graph_data(endpoint_name):
+    success,data = flask.returnDataCombinationPlot(endpoint_name)
+    print("data:")
+    print(data)
+    return json.dumps({'success':success,'data':data}),200,{'ContentType':'application/json'}
+    
 
