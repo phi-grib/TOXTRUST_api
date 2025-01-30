202d13fc8f0c58df00963325b559ff2c0ea47837
AdrianCabreraPhi <adriancabproven@gmail.com>
2025-01-30 14:28:09 +0100

    cors added

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "TOXTRUST"
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host="0.0.0.0",debug=True)
--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+UPLOAD_FOLDER = './'
+app = Flask(__name__,static_folder="static/dist",static_url_path="/")
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 url_base = "/toxtrust/"
 version = "v1/"
